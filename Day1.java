ca1509548efd1963b9e93d5730bfd94efe0a4a91
ShivaTamil22 <sivatamil2018@gmail.com>
2026-07-30 17:27:07 +0530

    Java Basics

---
+++ Day1.java
@@ -0,0 +1,38 @@
+class Day1 {
+    public static void main(String[] args) {
+        // System.out.println("Hello, World!");
+
+        // String Name = "Shiva";
+        // System.out.println("Hiiiiiii " + Name);
+
+        // int Age = 22;
+        // System.out.println("My Age is " + Age);
+
+        // long Mobile = 7010421573l;
+        // System.out.println("Mobile Number : " + Mobile);
+
+        // Float Balance = 566.32f;
+        // System.out.println("Your Account Balance is " + Balance);
+
+        // double Pi = 3.1415926535 ;
+        // System.out.println("Pi Value : " + Pi);
+
+        // byte age = 22;
+        // System.out.println("Your Age : " + age);
+
+        // short Marks = 1150;
+        // System.out.println("Total Marks : " + Marks);
+
+        char grade = 'A';
+        System.out.println("Your Grade : " + grade);
+
+        
+
+
+
+
+
+
+
+    }
+}
