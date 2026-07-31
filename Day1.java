958e375f86add8c9f5c770fa6af8295e6cf85e3a
ShivaTamil22 <sivatamil2018@gmail.com>
2026-07-31 15:04:33 +0530

    Day1 File Updated

--- Day1.java
+++ Day1.java
@@ -23,16 +23,98 @@ class Day1 {
         // short Marks = 1150;
         // System.out.println("Total Marks : " + Marks);
 
-        char grade = 'A';
-        System.out.println("Your Grade : " + grade);
+        // char grade = 'A';
+        // System.out.println("Your Grade : " + grade);
 
+        // // Camel Case Variable Name 
+        // String myName = "Shiva";
+        // System.out.println(myName);
+
+        // // Pascal Case Variable Name 
+        // String MyName = "Shiva";
+        // System.out.println(MyName);
+
+        // // Snake Case Variable Name 
+        // String my_name = "Shiva";
+        // System.out.println(my_name);
+
+        // String $name = "Shiva";
+        // String _name = "Tamil";
+        // System.out.println($name + " " + _name);
+
+        // Arithmetic Operators (+, -, *, /, %)
+
+        // int num1 = 10;
+        // int num2 = 2;
+        // System.out.println(num1 + "+" + num2 + " = " + (num1+num2));
+        // System.out.println(num1 + "-" + num2 + " = " + (num1-num2));
+        // System.out.println(num1 + "*" + num2 + " = " + (num1*num2));
+        // System.out.println(num1 + "/" + num2 + " = " + (num1/num2));
+        // System.out.println(num1 + "%" + num2 + " = " + (num1%num2));
+
+        // Assignment Operator
+
+        // int a = 20;
+
+        // a += 10;
+        // a -= 5;
+        // a *= 2;
+        // a /= 4;
+        // a %= 3;
+        // System.out.println(a);
+
+        // Relational Operator (Comparison) ==, !=, <, >, <=, >=
+
+        // int a = 20;
+        // int b = 10;
+        // System.out.println(a == b);
+        // System.out.println(a != b);
+        // System.out.println(a <= b);
+        // System.out.println(a >= b);
+        // System.out.println(a < b);
+        // System.out.println(a > b);
+
+        // Logical Operators (&& , || )
+
+        // int a = 10 ;
+        // int b = 5 ;
+
+        // System.out.println(a>b && a != b);
+        // System.out.println(a>b || a == b);
+        // System.out.println(a<b && a != b);
+        // System.out.println(a<b || a == b);
+
+        // increment decrement (Pre ++i, post i--)
+
+        // int a = 5;
+        // int b = ++a;
+        // System.out.println(a);
+        // System.out.println(b);
+
+        // int a = 5;
+        // int b = --a;
+        // System.out.println(a);
+        // System.out.println(b);
+
+        // int a = 5;
+        // int b = a++;
+        // System.out.println(a);
+        // System.out.println(b);
+
+        // int a = 5;
+        // int b = a--;
+        // System.out.println(a);
+        // System.out.println(b);
+
+
+
+        //  Unary operator
+
+        // int a = 5;
         
-
-
-
-
-
-
+        // System.out.println(-a);
+        // System.out.println(!(a == 10));
+        
 
     }
 }
