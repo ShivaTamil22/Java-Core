8c133a991c980cffc433c3ed23e8f8a45134e265
ShivaTamil22 <sivatamil2018@gmail.com>
2026-07-31 16:48:11 +0530

    Day2 Completed

---
+++ Day2.java
@@ -0,0 +1,134 @@
+class Day2 {
+    public static void main (String[] args){
+
+        // // totol and Average of marks
+        // int T=90, E=70 , M=90, S=100, SS=100;
+        // int Total = T+E+M+S+SS;
+        // int Average = Total/5;
+        // System.out.println("total Marks : " + Total );
+        // System.out.println("Average Of Marks : " + Average);
+
+
+        // Swap two Numbers
+
+        // int a = 10;
+        // int b = 20;
+
+        // System.out.println("Before Swapping");
+        // System.out.println("A : " +a);
+        // System.out.println("B : " + b);
+
+        // a = a + b; // a = 30
+        // b = a - b; // b = 10
+        // a = a - b; // a = 20
+
+        // System.out.println("After Swapping");
+        // System.out.println("A : " +a);
+        // System.out.println("B : " + b);
+
+
+        // Fahrenheit To celsius
+
+        // double F = 75;
+        // double C ;
+
+        // C =  (F - 32) * 5 / 9;
+
+        // System.out.println("Fahrenheit : " + F);
+        // System.out.println("Celsius : " + C);
+
+        // // Speed Calculator
+        // double speed;
+        // double distance = 100;
+        // double time = 2;
+
+        // speed = distance/time;
+        // System.out.println("Distance :" + distance + "Km.");
+        // System.out.println("Time :" + time +"hrs." );
+        // System.out.println("Speed :" + speed +"Km/hr.");
+
+
+        // IF Condition
+
+        // int mark = 20;
+        // if(mark>=35){
+        //     System.out.println("Student Pass");
+        // }
+        // if (mark<35 ){
+        //     System.out.println("Student Fail");
+        // }
+
+        // If Else
+        // int age = 22;
+        // if(age>=18){
+        //     System.out.println("Eligible to Vote");
+        //     }
+        // else{
+        //     System.out.println("Eligible to Vote");
+        //     }
+
+        //else if 
+
+        // int marks = 90; 
+        // if (marks>= 90){
+        //     System.out.println("Grade A");
+        // }
+        // else if (marks>= 80){
+        //     System.out.println("Grade B");
+        // }
+        // else if (marks>= 60){
+        //     System.out.println("Grade C");
+        // }
+        // else if (marks>= 35){
+        //     System.out.println("Grade D");
+        // }
+        // else{
+        //     System.out.println("Fail");
+        // }
+
+        // nested if
+
+        // int a = 70 ;
+        // int b = 70 ;
+        // int c = 60 ;
+
+        // if(a>b){
+        //     if(a>c){System.out.println("A is Bigget Value");}
+        // }
+        // if(b>a){
+        //     if(b>c){System.out.println("B is Bigget Value");}
+        // }
+        // if(c>a){
+        //     if(c>b){System.out.println("C is Bigget Value");}
+        // }
+        // else {System.out.println("All values are equal");}
+
+        // Switch Statement 
+
+        // int a = 20;
+        // int b = 5;
+        
+        // String action = "l";
+        // switch(action){
+        //     case "+":
+        //         System.out.println(a + b);
+        //         break;
+        //         case "-":
+        //         System.out.println(a - b);
+        //         break;
+        //         case "*":
+        //         System.out.println(a * b);
+        //         break;
+        //         case "/":
+        //         System.out.println(a / b);
+        //         break;
+        //         default :
+        //         System.out.println("Invalid Action");
+        // }
+
+    //     int age = 22;
+    //     String E = (age >= 18) ? "Eligible" : "Not Eligible";
+    //     System.out.println("Vote Eligibility : " + E);
+
+    // }
+}
