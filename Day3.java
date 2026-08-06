7139441b76236b40404c3615d6025cdd78c56c40
ShivaTamil22 <sivatamil2018@gmail.com>
2026-08-06 13:05:43 +0530

    Day updated

---
+++ Day3.java
@@ -0,0 +1,128 @@
+class Day3 {
+    public static void main (String []args){
+
+        // // Do while
+        // int i = 1;
+        // do{
+        //     System.out.println(i);
+        //     System.out.println("Hii Shiva");
+        //     i++;
+        // } while (i<=100);
+        
+        // While Loop
+        // int i = 1;
+        // while(i<=5){
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+        // For Loop
+        
+        // for (int i = 1; i <=5;i++){
+        //     System.out.println(i);
+        // }
+
+        // for (char i='A' ; i<='Z'; i++){
+        //     // System.out.println(i);
+        //     System.out.print( i + " ");
+        // }
+
+        // print 5 to 1
+        // int i = 5;
+        // while (i>=1){
+        //     System.out.println(i);
+        //     i--;
+        // }
+        // for (int i = 5; i>=1;i--){
+        //     System.out.println(i);
+        // }
+
+        // Sum of 10 numers
+        // int sum = 0;
+        // for (int i = 1; i<=10; i++){
+        //     sum += i;
+        //     System.out.println(sum);
+        // }
+
+        // Multiplication Table
+        // for (int i = 1;i <=10;i++){
+        //     System.out.println(i + " * = " + i*5);
+        // }
+
+        // for (int i = 1;i <=10;i++){
+        //     System.out.println(i + " * = " + i*10);
+        // }
+
+        // Square Numbers
+
+        // for (int i = 1; i<=10; i++){
+        //     System.out.println(i + " Square value : " + i*i);
+        // }
+
+        // Factorial
+        // int fact = 1;
+        // for (int i =1;i<=5;i++){
+        //     fact *= i;
+        // }
+        // System.out.println(fact);
+
+        // // Even Number
+        // for (int i = 1; i <=10; i++){
+        //     if (i % 2 == 0){
+        //         System.out.println(i + " Even number");
+        //     }
+        // }
+
+
+        // int i = 1;
+        // while(i <= 10){
+        //     if (i %2 == 0){
+        //         System.out.println(i + " Even number");
+        //     }
+        //     i++;
+        // }
+
+        // Odd Numbers
+
+        // for (int i = 1; i<=10; i++){
+        //     if (i % 2 != 0){
+        //         System.out.println(i +" - Odd Number");
+        //     }
+        // }
+
+
+        // Break and Continue
+        // for (int i=1; i<=10;i++){
+        //     if (i==5){
+        //         break;
+        //     }
+        //     System.out.println(i);
+        // }
+        // for (int i = 1 ; i<=10 ; i++){
+        //     if (i ==5){
+        //         continue;
+        //     }
+        //     System.out.println(i);
+        // }
+
+        // Nested For Loop 
+        // for (int i = 1; i <= 10; i++){
+        //     for (int j = 1 ; j<=10; j++){
+        //         System.out.print(i+ " ");
+        //     }
+        //     System.out.println(" ");
+        // }
+
+        // for (int i = 10; i >= 1; i--){
+        //     for (int j = 10 ; j>=i; j--){
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println(" ");
+        // }
+
+        
+       
+
+
+}
+}
