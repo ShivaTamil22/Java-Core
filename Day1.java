3d337bce5aadda50f3c2535609d1fab094e34da0
ShivaTamil22 <sivatamil2018@gmail.com>
2026-08-17 14:03:46 +0530

    Updated

--- Day1.java
+++ Day1.java
@@ -1,4 +1,4 @@
-class Day1 {
+public class Day1 {
     public static void main(String[] args) {
         // System.out.println("Hello, World!");
 
---
+++ Day6.java
@@ -0,0 +1,85 @@
+public class Day6{
+    public static void main (String[] args){
+        // StringBuffer sb = new StringBuffer("Hello Shiva");
+        
+        // sb.append(" Shiva");
+        // sb.insert(5, " Shiva");
+        // sb.replace(6, 11, "Prem");
+        // sb.delete(6, 11);
+        // sb.deleteCharAt(2);
+        // sb.reverse();
+        // sb.setCharAt(0, 'F');
+        // sb.setLength(4);
+        // sb.toString();
+        // System.out.println(sb);
+
+        // System.out.println(sb.substring(0,5));
+        // System.out.println(sb.charAt(6));
+        // System.out.println(sb.subSequence(0,5));
+        // System.out.println(sb.indexOf("llo"));
+        // System.out.println(sb.lastIndexOf("iva"));
+        // System.out.println(sb.length());
+        // System.out.println(sb.capacity());
+        // sb.ensureCapacity(100);
+        // System.out.println(sb.capacity());
+
+        // sb.trimToSize();
+        // System.out.println(sb.capacity());
+
+
+
+        // -------------------------------------------------------
+        
+        StringBuilder sb = new StringBuilder ("Hello Shiva");
+
+        
+        // sb.append(" Shiva");
+
+        // sb.insert(5, " Shiva");
+
+        // sb.replace(6, 11, "Prem");
+
+        // sb.delete(6, 11);
+
+        // sb.deleteCharAt(2);
+
+        // sb.reverse();
+
+        // sb.setCharAt(0, 'F');
+
+        // sb.setLength(4);
+
+        // sb.toString();
+
+        // System.out.println(sb);
+
+
+
+
+
+        // System.out.println(sb.substring(0,5));
+
+        // System.out.println(sb.charAt(6));
+
+        // System.out.println(sb.subSequence(0,5));
+
+        // System.out.println(sb.indexOf("llo"));
+
+        // System.out.println(sb.lastIndexOf("iva"));
+
+        // System.out.println(sb.length());
+
+        // System.out.println(sb.capacity());
+
+        // sb.ensureCapacity(100);
+
+        // System.out.println(sb.capacity());
+
+        // sb.trimToSize();
+        // System.out.println(sb.capacity());
+        
+
+
+        
+    }
+}
