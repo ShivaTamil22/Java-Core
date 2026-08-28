34fc7a3d90e0b7dc674e865ff997dff2a9e0dbd5
ShivaTamil22 <sivatamil2018@gmail.com>
2026-08-28 17:14:45 +0530

    Day5 Math updated

--- Day5_math.java
+++ Day5_math.java
@@ -1,6 +1,66 @@
 public class Day5_math {
     public static void main(String[] args){
+
+        // System.out.println(Math.abs(10-20));
         
+        int a = 10;
+        int b = 20;
+
+        // System.out.println("Max Value : " + Math.max(a, b));
+        // System.out.println("Min Value : " +Math.min(a, b));
+
+
+        // System.out.println(Math.sqrt(64));
+
+        // System.out.println(Math.pow(10, 3));
+
+        // System.out.println(Math.cbrt(64));
+        // System.out.println(Math.ceil(10.2));
+        // System.out.println(Math.ceil(10.0));
+        // System.out.println(Math.ceil(10.9));
+        // System.out.println(Math.round(22.2));
+        // System.out.println(Math.round(22.7));
+        // System.out.println(Math.floor(25.1));
+        // System.out.println(Math.floor(25.9));
+        // System.out.println(Math.random());
+
+        // System.out.println(Math.PI);
+        // System.out.println(Math.E);
+
+        // Area of circle
+
+        // double  r = 10;
+
+        // float A = (float)(Math.PI * Math.pow(r, 2));
+        // System.out.println("Radius : " + r);
+        // System.out.println("Area of circle : " + A);
+
+        // equilateral triangle
+
+        // int angle = 10;
+        // float Area = (float)((Math.sqrt(3)*(angle*angle))/4);
+        // System.out.println(Area);
+        // System.out.println(Math.round(Area));
+
+
+
+
+
+
+
+
+        // Math Tasks
+        // System.out.println(Math.sin(Math.toRadians(90)));
+        // System.out.println(Math.cos(Math.toRadians(90)));
+        // System.out.println(Math.tan(Math.toRadians(90)));
+        // System.out.println(Math.toDegrees(Math.PI));
+        // System.out.println(Math.toDegrees(Math.PI*2));
+        // System.out.println(Math.toDegrees(Math.PI/2));
+        // System.out.println(Math.toDegrees(Math.PI/4));
+
+
+        
+
     }
     
 }
