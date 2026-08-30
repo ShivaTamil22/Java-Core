707ff5a2fa94b17f6353dbd92fe8ffc119a674d1
ShivaTamil22 <sivatamil2018@gmail.com>
2026-08-30 21:34:05 +0530

    Day8 updated

---
+++ Day8.java
@@ -0,0 +1,108 @@
+public class Day8 {
+    public static void main(String[] args) {
+        // int [] marks = { 100, 90, 80,90,100};
+        // System.out.println(marks [0]);
+        // System.out.println(marks [1]);
+        // marks[0]= 90;
+        // System.out.println(marks [0]);
+
+        // for (int i = 0; i < marks.length; i++ ){
+        //     System.out.println(marks[i]);
+        // }
+
+        // String[] names = {"ram", "shiva", "kumar", "sathish"};
+        // for (int i = 0; i < names.length; i++ ){
+        //     System.out.println(names[i]);
+        // }
+
+        // int[] numbers = {10,20,30,40,50,60,70,80,90,100};
+        // int sum = 0;
+        // for (int i = 0; i < numbers.length;i++){
+        //     sum = sum + numbers[i];
+        //     System.out.println("Steps : " + sum);
+        // }
+        // System.out.println("Answer : " + sum);
+
+        // int [] marks = { 100, 90, 80,90,100};
+        // int sum = 0;
+        // for (int i = 0; i<marks.length;i++){
+        //     sum = sum + marks[i];
+        //     System.out.println("Steps : " + sum);
+        // }
+        // System.out.println("Total Marks : " + sum);
+        // double avg = sum / marks.length;
+        // System.out.println("average : " + avg );
+
+        // Largest number
+
+        // int[] numbers = {10,70,60,80,40,20};
+        // int largest = numbers[0];
+        // for (int i = 1; i < numbers.length;i++){
+        //     if (numbers[i]>largest) {
+        //         largest = numbers[i];
+        //         System.out.println("Steps : " + largest);
+        //     }
+        // }
+        // System.out.println("Largest Number : " + largest);
+        // int smallest = numbers[0];
+        // for (int i = 1; i <numbers.length; i++){
+        //     if (numbers[i]<smallest) {
+        //         smallest = numbers[i];
+        //         System.out.println("Steps : " + smallest);
+        //     }
+        // }
+        // System.out.println("Smallest Number : " + smallest);
+
+
+        // Odd or Even
+
+        // int[] numbers = { 1, 12,15,16,22,34,35,65,26};
+        // for (int i = 0; i < numbers.length; i++){
+        //     if (numbers[i] % 2 == 0) {
+        //         System.out.println(numbers[i] +" is Even Number");
+        //     }
+        //     else {
+        //         System.out.println(numbers[i] + " is Odd Number ");
+        //     }
+        // }
+
+        // ForEach
+        // int [] numbers = {1,3,2,42,23,32,36};
+        // for (int number:numbers){
+        //     System.out.println(number);
+        // }
+
+        // 2D Array
+        int [] [] numbers = {
+            {10,20,30},
+            {40,50,60},
+            {70,80,90}
+        };
+        // for (int i =0;i<numbers.length;i++){
+        //     for (int j =0;j<numbers[i].length;j++){
+        //         System.out.print(numbers[i][j] + " ");
+        //     }
+        //     System.out.println("");
+        // }
+
+        // System.out.println(numbers[0][0]);
+        // System.out.println(numbers[0][2]);
+        // System.out.println(numbers[2][0]);
+        // System.out.println(numbers[0][1]);
+        // System.out.println(numbers[0][2]);
+        
+
+
+
+        // Array Tasks
+
+        
+
+
+
+        
+
+
+    }
+    
+}
