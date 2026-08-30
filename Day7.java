799a8c3b134d4433c16c313e4afb1b9248486975
ShivaTamil22 <sivatamil2018@gmail.com>
2026-08-30 19:34:08 +0530

    DAY 7 updated

---
+++ Day7.java
@@ -0,0 +1,85 @@
+public class Day7 {
+
+    // static void display(){
+    //     System.out.println("Welcome to java Programming");
+    // }
+
+    // Without arg ,without return type
+
+    // static void add(){
+    //     int a = 10;
+    //     int b = 20;
+    //     System.out.println(a+b);
+    // }
+
+    // With arg ,without return type
+    // static void add(int a, int b, int c ){
+    //     System.out.println(a+b+c);
+    // }
+
+    // Without arg ,with return type
+    // static int add(){
+    //     int a = 10;
+    //     int b = 20;
+    //     return (a+b);
+    // }
+
+    // With arg ,with return type
+    // static int add(int a, int b, int c, int d, int e){
+    //     return (a+b+c+d+e);
+    // }
+
+    // static void greet(String fName, String lName ){
+    //     System.out.println("Welcome " + fName +" "+ lName);
+    // }
+
+    // static void greet(String fName, String lName, int id ){
+    //     System.out.println("Welcome " + fName +" "+ lName);
+    //     System.out.println("ID : " + id);
+    // }
+
+    // static void checkAge(int age){
+    //     if (age < 18){
+    //         System.out.println("Not Eligible to vote");
+    //     }
+    //     else {
+    //         System.out.println("Eligible to Vote");
+    //     }
+    // }
+
+    // static int add(int a,int b){
+    //     return (a+b);
+
+    // }
+    // static double add(double a,double b){
+    //     return (a+b);
+
+    // }
+    // static String add(String a,String b){
+    //     return a+b;
+    // }
+
+
+
+    
+
+    public static void main (String[] args){
+        // display();
+        // add();
+        // add(50, 30, 20);
+        // System.out.println(add());
+        // System.out.println(add(90, 100, 80, 90, 100));
+
+        // greet("Shiva", "Tamil");
+        // greet("Shiva", "Tamil" , 202);
+
+        // checkAge(22);
+
+        // System.out.println(add(4,7));
+        // System.out.println(add(50.78, 50.90));
+        // System.out.println(add("Shiva", "Tamil"));
+    }
+    
+}
+
+
