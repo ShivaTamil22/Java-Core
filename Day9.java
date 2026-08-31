c2f58f979df2081b08c00fc82f8ce866d5e9fa8f
ShivaTamil22 <sivatamil2018@gmail.com>
2026-08-31 09:00:47 +0530

    Day9 updated

---
+++ Day9.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class Day9 {
+
+    public static void main (String args []){
+        Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter the Number ");
+        // int number = sc.nextInt();
+        // System.out.println("Number : " + number);
+
+        // System.out.println("Enter Name ");
+        // String name = sc.next();
+        // System.out.println("Hi "+ name);
+
+        // Add Two Numbers
+
+    //     System.out.println( "Enter Two Numbers");
+    //     int a = sc.nextInt();
+    //     int b = sc.nextInt();
+    //     int sum = a+b;
+    //     System.out.println("Sum Of "+ a+ " And" +b+ " is " + sum );
+
+    // Odd or Even Number
+    // System.out.println("Enter the Number ");
+    // int num = sc.nextInt();
+    // if (num == 0) {
+    //     System.out.println("Value is Zero");
+    // }
+    // else if (num%2==0) {
+    //     System.out.println(num + " - is Even Number");
+    // }
+    // else {
+    //     System.out.println(num + " - is Odd Number");
+    // }
+
+    // Positive or Negative
+    // System.out.println("Enter the number");
+    // int num = sc.nextInt();
+    // if (num > 0) {
+    //     System.out.println(num +" is a Positive Number");
+    // }
+    // else if (num<0) {
+    //     System.out.println(num +" is a Negative Number");
+    // }
+    // else{
+    //     System.out.println(num +" is a Zero");
+    // }
+
+
+
+    }
+
+
+}
---
+++ Day9P1.java
@@ -0,0 +1,62 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Day9P1 {
+    public static void main ( String args[]){
+    //     try {
+    //         File file = new File("Shiva.txt");
+    //         if (file.createNewFile()){
+    //             System.out.println("File Created Successfully");
+    //         } else {
+    //             System.out.println("File already exist");
+    //         }
+    //     } catch (IOException e){
+    //         System.out.println("Error : " + e.getMessage());
+    //     }
+
+    // try {
+    //     FileWriter writer = new FileWriter("Shiva.txt");
+    //     writer.write("Welcome to Java Session");
+    //     writer.close();
+    //     System.out.println("Data written Successfully");
+    // } catch (IOException e){
+    //     System.out.println("Error : " +e.getMessage());
+    // }
+
+    // File file = new File("Shiva.txt");
+    // System.out.println("File Name :" + file.getName());
+    // System.out.println("File Path :" + file.getAbsolutePath());
+    // System.out.println("File Name :" + file.getPath());
+    // System.out.println("File Size :" + file.length());
+    // if (file.delete()) {
+    //     System.out.println("File Deleted");
+        
+    // } else {
+    //     System.out.println("File Not exist");
+    // }
+
+    // try {
+    //      File file = new File("Shiva.txt");
+    // if (file.createNewFile()) {
+    //     System.out.println("File created");
+    // } else {
+    //     System.out.println("File Already Exist");
+    // }
+    // } catch (IOException e){
+    //     System.out.println(e.getMessage());
+    // }
+   
+    
+
+    
+
+
+
+
+    }
+
+
+    
+}
